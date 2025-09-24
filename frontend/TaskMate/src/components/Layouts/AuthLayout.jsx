4d5bdf0b039cf90936baf30f076c0a1de3fe1bec
ybthummar <yugthummar001@gmail.com>
2025-09-25 00:47:27 +0530

    updated ui

--- frontend/TaskMate/src/components/Layouts/AuthLayout.jsx
+++ frontend/TaskMate/src/components/Layouts/AuthLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import UI_IMG from '../../assets/auth-img.png'
+import UI_IMG from '../../assets/auth-img.jpeg'
 export const AuthLayout = ({children}) => {
   return <div className='flex'>
     <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
@@ -7,7 +7,7 @@ export const AuthLayout = ({children}) => {
         {children}
     </div>
 
-    <div className='hidden md:flex w-[40vw] h-screen item-center justify-center bg-blue-50 bg-no-repeat bg-center overflow-hidden lg:w-[90%]'>
+    <div className='hidden md:flex w-[40vw] h-screen item-center justify-center bg-blue-50 lg:w-[90%]'>
         <img src={UI_IMG} alt="auth-img" className=''/>
     </div>
   </div>
